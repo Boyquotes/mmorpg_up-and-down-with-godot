7fb6df157ff796e14827e5d03887ffeecee084c9
DESKTOP-OM358TJ\Olionnn <nayasasuke12@gmail.com>
2022-03-29 15:46:44 +0700

    adding animation player

--- Player.gd
+++ Player.gd
@@ -6,6 +6,9 @@ const FRICTION = 400
 
 var velocity = Vector2.ZERO
 
+onready var animationPlayer = $AnimationPlayer
+
+
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
@@ -13,8 +16,17 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		if input_vector.x > 0:
+			animationPlayer.play("RunRight")
+		else:
+			animationPlayer.play("RunLeft")
+		if input_vector.y > 0:
+			animationPlayer.play("RunDown")
+		else:
+			animationPlayer.play("RunUp")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationPlayer.play("IdleLeft")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
